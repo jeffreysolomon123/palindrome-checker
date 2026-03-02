dfe93bbd6b7f2392a6daa50a863a82bc88ffa8b8
Jeffrey Solomon <jeffreysolomon.official@gmail.com>
2026-03-02 22:42:58 +0530

    UC5: Add case insensitive palindrome

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -7,6 +7,11 @@ public class PalindromeChecker {
         return word.equals(reversed);
     }
 
+    public static boolean isCaseInsensitivePalindrome(String word) {
+        String reversed = new StringBuilder(word).reverse().toString();
+        return word.equalsIgnoreCase(reversed);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("=== Palindrome Checker App ===");
@@ -16,8 +21,8 @@ public class PalindromeChecker {
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        if (isSimplePalindrome(word)) {
-            System.out.println(word + " is a palindrome");
+        if (isCaseInsensitivePalindrome(word)) {
+            System.out.println(word + " is a palindrome (case-insensitive)");
         } else {
             System.out.println(word + " is not a palindrome");
         }
