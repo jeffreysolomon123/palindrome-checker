842720a67cb0b88f28bb178dc0519e6eb7e38204
Jeffrey Solomon <jeffreysolomon.official@gmail.com>
2026-03-02 22:40:53 +0530

    UC2: Hardcoded palindrome check

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,5 +1,16 @@
 public class PalindromeChecker {
+
     public static void main(String[] args) {
+
         System.out.println("=== Palindrome Checker App ===");
+
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println("madam is a palindrome");
+        } else {
+            System.out.println("madam is not a palindrome");
+        }
     }
 }
