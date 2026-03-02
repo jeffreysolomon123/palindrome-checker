8e8e0e252b865705b004310b83803ce09d0ae87a
Jeffrey Solomon <jeffreysolomon.official@gmail.com>
2026-03-02 22:42:27 +0530

    UC4: Add simple palindrome method

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
 
+    public static boolean isSimplePalindrome(String word) {
+        String reversed = new StringBuilder(word).reverse().toString();
+        return word.equals(reversed);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("=== Palindrome Checker App ===");
@@ -11,9 +16,7 @@ public class PalindromeChecker {
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        String reversed = new StringBuilder(word).reverse().toString();
-
-        if (word.equals(reversed)) {
+        if (isSimplePalindrome(word)) {
             System.out.println(word + " is a palindrome");
         } else {
             System.out.println(word + " is not a palindrome");
